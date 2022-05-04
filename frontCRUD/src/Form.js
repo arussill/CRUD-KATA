18ff64d00ef8e7172857b10a7ab3fca7b7caab34
Aura Russill <auracris1996@hotmail.com>
2022-05-03 23:21:25 -0500

    REFACTOR - se dividio el codigo en diferentes archivos

---
+++ frontCRUD/src/Form.js
@@ -0,0 +1,85 @@
+import React, {
+  useContext, useRef,
+  useState
+} from "react";
+import { Store, HOST_API } from "./App";
+
+export const Form = () => {
+  const formRef = useRef(null);
+  const {
+    dispatch, state: { item },
+  } = useContext(Store);
+  const [state, setState] = useState({ item });
+
+  const onAdd = (event) => {
+    // event.preventDefault();
+    const request = {
+      name: state.name,
+      id: null,
+      isCompleted: false,
+    };
+
+    fetch(HOST_API + "/todo", {
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((todo) => {
+        dispatch({ type: "add-item", item: todo });
+        setState({ name: "" });
+        formRef.current.reset();
+      });
+  };
+
+  const onEdit = (event) => {
+    // event.preventDefault();
+    const request = {
+      name: state.name,
+      id: item.id,
+      isCompleted: item.isCompleted,
+    };
+
+    fetch(HOST_API + "/todo", {
+      method: "PUT",
+      body: JSON.stringify(request),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((todo) => {
+        dispatch({ type: "update-item", item: todo });
+        setState({ name: "" });
+        formRef.current.reset();
+      });
+  };
+
+  return (
+    <form ref={formRef} className="input-group">
+      <input
+
+        className="form-control"
+        placeholder="Escriba una tarea"
+        type="text"
+        name="name"
+        defaultValue={item.name}
+        onChange={(event) => {
+          setState({ ...state, name: event.target.value });
+        }}
+      ></input>
+      {item.id && (
+        <button class="btn btn-dark" onClick={onEdit}>
+          Actualizar
+        </button>
+      )}
+      {!item.id && (
+        <button class="btn btn-dark" onClick={onAdd}>
+          Agregar
+        </button>
+      )}
+    </form>
+  );
+};
---
+++ frontCRUD/src/List.js
@@ -0,0 +1,94 @@
+import React, {
+  useContext,
+  useEffect
+} from "react";
+import { Store, HOST_API } from "./App";
+
+export const List = () => {
+  const { dispatch, state } = useContext(Store);
+
+  useEffect(() => {
+    fetch(HOST_API + "/todos")
+      .then((response) => response.json())
+      .then((list) => {
+        dispatch({ type: "update-list", list });
+      });
+  }, [state.list.length, dispatch]);
+
+  const onDelete = (id) => {
+    fetch(HOST_API + "/" + id + "/todo", {
+      method: "DELETE",
+    }).then((list) => {
+      dispatch({ type: "delete-item", id });
+    });
+  };
+
+  const onEdit = (todo) => {
+    dispatch({ type: "edit-item", item: todo });
+  };
+
+  const onChange = (event, todo) => {
+    const request = {
+      name: todo.name,
+      id: todo.id,
+      completed: event.target.checked,
+    };
+    fetch(HOST_API + "/todo", {
+      method: "PUT",
+      body: JSON.stringify(request),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((todo) => {
+        dispatch({ type: "update-item", item: todo });
+      });
+  };
+
+  return (
+    <div className="tabla">
+      <table className="table table-striped">
+        <thead className="thead-light">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Tarea</th>
+            <th scope="col">Completado</th>
+            <th scope="col">Eliminar</th>
+            <th scope="col">Editar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.list.map((todo) => (
+            <tr>
+              <th scope="row">{todo.id}</th>
+              <td className={todo.completed ? "tachado" : {}}>{todo.name}</td>
+              <td>
+                <input
+                  type="checkbox"
+                  defaultChecked={todo.completed}
+                  onChange={(event) => onChange(event, todo)}
+                ></input>
+              </td>
+              <td>
+                <button
+                  type="button"
+                  class="btn btn-danger"
+                  onClick={() => onDelete(todo.id)}
+                >
+                  Eliminar
+                </button>
+              </td>
+
+              <td>
+                <button class="btn btn-warning" onClick={() => onEdit(todo)}>
+                  Editar
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
